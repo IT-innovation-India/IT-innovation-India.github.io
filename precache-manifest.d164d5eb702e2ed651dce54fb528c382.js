ab79c9b1834062e316142d136c5f7bb3e5cf26aa
dineshk <dinesh.kabade@iti-global.net>
2020-01-16 12:37:15 +0530

    Latest code updating

---
+++ precache-manifest.d164d5eb702e2ed651dce54fb528c382.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81fb9e0ea2542abbeef8b6c1ab5c69e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c8ab1ee1516786fffba",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "461c21c075ac2c2337f2",
+    "url": "/static/css/main.eac04910.chunk.css"
+  },
+  {
+    "revision": "3c8ab1ee1516786fffba",
+    "url": "/static/js/2.b815ce00.chunk.js"
+  },
+  {
+    "revision": "d9f68a349bb7d589151d4face76bac2f",
+    "url": "/static/js/2.b815ce00.chunk.js.LICENSE"
+  },
+  {
+    "revision": "461c21c075ac2c2337f2",
+    "url": "/static/js/main.51910cea.chunk.js"
+  },
+  {
+    "revision": "8ad71b5583c162c75b4c",
+    "url": "/static/js/runtime-main.5d7141c9.js"
+  },
+  {
+    "revision": "8e6ffd2622fc27bbd1de3f01fae5d944",
+    "url": "/static/media/activity.8e6ffd26.jpg"
+  },
+  {
+    "revision": "088aa7235a34eee29370d63ca71b5fd9",
+    "url": "/static/media/class.088aa723.jpg"
+  },
+  {
+    "revision": "1fa81a89b0ca0b02cb3e5ee2784a81bc",
+    "url": "/static/media/iti-crossculture.1fa81a89.jpg"
+  },
+  {
+    "revision": "9b6e22e3f489ee41f5277cdf4ee0d1be",
+    "url": "/static/media/iti-gdp.9b6e22e3.png"
+  },
+  {
+    "revision": "14d34604aeaec72c6edff29e20f46e4b",
+    "url": "/static/media/iti-growtogether.14d34604.jpg"
+  },
+  {
+    "revision": "67d832bdb07dca3f815f93b56fe6d4ac",
+    "url": "/static/media/iti-industries.67d832bd.jpeg"
+  },
+  {
+    "revision": "bc7b2bcb37aa86d80896d868bfc748e3",
+    "url": "/static/media/iti-leadership.bc7b2bcb.jpg"
+  },
+  {
+    "revision": "b7b958551d5259e3cb194657a1bbdbaf",
+    "url": "/static/media/iti-logo.b7b95855.JPG"
+  },
+  {
+    "revision": "628b4daf785276156bc6d568b5152dc7",
+    "url": "/static/media/iti-map.628b4daf.png"
+  },
+  {
+    "revision": "e5cb12f1d86827d1c3db9d848537a69b",
+    "url": "/static/media/iti-pune.e5cb12f1.jpeg"
+  },
+  {
+    "revision": "804e63e7ce3f216be05bd857f80a6e7d",
+    "url": "/static/media/iti-puneUni.804e63e7.jpeg"
+  },
+  {
+    "revision": "2dc5187729f1e67fe83c3b601d670307",
+    "url": "/static/media/m1.2dc51877.jpg"
+  },
+  {
+    "revision": "909ecb873b6b313d5554e339f2f15066",
+    "url": "/static/media/m2.909ecb87.png"
+  },
+  {
+    "revision": "98940c88b11c89bc57bc1bcfdf6d5bbf",
+    "url": "/static/media/practical.98940c88.jpg"
+  },
+  {
+    "revision": "bc39a410404df97c8607f96f260648de",
+    "url": "/static/media/qua1.bc39a410.jpg"
+  },
+  {
+    "revision": "8bb59d85e0225098a7bb4705e24ecc3a",
+    "url": "/static/media/quality.8bb59d85.png"
+  }
+]);
