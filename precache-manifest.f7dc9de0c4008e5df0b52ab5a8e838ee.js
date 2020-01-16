f1f6826e4880de4d63c32413f1accbd2530ac8eb
dineshk <dinesh.kabade@iti-global.net>
2020-01-16 11:35:50 +0530

    Second version

---
+++ precache-manifest.f7dc9de0c4008e5df0b52ab5a8e838ee.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9b829196c3be33b1a09a15d4a36c2b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e73de255d551177ffb0",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "43c5ac5a68291f81ab52",
+    "url": "/static/css/main.dd1b9e0d.chunk.css"
+  },
+  {
+    "revision": "4e73de255d551177ffb0",
+    "url": "/static/js/2.dc17aaad.chunk.js"
+  },
+  {
+    "revision": "d9f68a349bb7d589151d4face76bac2f",
+    "url": "/static/js/2.dc17aaad.chunk.js.LICENSE"
+  },
+  {
+    "revision": "43c5ac5a68291f81ab52",
+    "url": "/static/js/main.bb4cf531.chunk.js"
+  },
+  {
+    "revision": "8ad71b5583c162c75b4c",
+    "url": "/static/js/runtime-main.5d7141c9.js"
+  },
+  {
+    "revision": "1fa81a89b0ca0b02cb3e5ee2784a81bc",
+    "url": "/static/media/iti-crossculture.1fa81a89.jpg"
+  },
+  {
+    "revision": "9b6e22e3f489ee41f5277cdf4ee0d1be",
+    "url": "/static/media/iti-gdp.9b6e22e3.png"
+  },
+  {
+    "revision": "14d34604aeaec72c6edff29e20f46e4b",
+    "url": "/static/media/iti-growtogether.14d34604.jpg"
+  },
+  {
+    "revision": "67d832bdb07dca3f815f93b56fe6d4ac",
+    "url": "/static/media/iti-industries.67d832bd.jpeg"
+  },
+  {
+    "revision": "bc7b2bcb37aa86d80896d868bfc748e3",
+    "url": "/static/media/iti-leadership.bc7b2bcb.jpg"
+  },
+  {
+    "revision": "b7b958551d5259e3cb194657a1bbdbaf",
+    "url": "/static/media/iti-logo.b7b95855.JPG"
+  },
+  {
+    "revision": "628b4daf785276156bc6d568b5152dc7",
+    "url": "/static/media/iti-map.628b4daf.png"
+  },
+  {
+    "revision": "e5cb12f1d86827d1c3db9d848537a69b",
+    "url": "/static/media/iti-pune.e5cb12f1.jpeg"
+  },
+  {
+    "revision": "804e63e7ce3f216be05bd857f80a6e7d",
+    "url": "/static/media/iti-puneUni.804e63e7.jpeg"
+  }
+]);
